a8ff51cf18e92a21ae892778654616e0e96bc57b
Pete Wall pwall@pivotal.io <pwall@pivotal.io>
2020-01-27 10:18:26 -0600

    JEB-ify helm style generator

--- helm/templates/run.sh
+++ helm/templates/run.sh
@@ -2,15 +2,15 @@
 
 source ./steps.sh
 
-if ! requirements_check; then exit 1; fi
-if ! log_existing_dependencies; then exit 1; fi
-if ! init_helm; then exit 1; fi
+if ! show_image_dependencies  ; then exit 1; fi
+if ! check_needs              ; then exit 1; fi
+if ! init_helm                ; then exit 1; fi
 if ! install_helm_chart /input/helm-chart test-instance; then exit 1; fi
 
 # The helm chart is now installed with the instance named 'test-instance'
 # Add your test steps here.
 
 if ! delete_helm_chart test-instance; then exit 1; fi
-if ! remove_helm; then exit 1; fi
+if ! remove_helm              ; then exit 1; fi
 
 echo "<%= testName %> succeeded"
